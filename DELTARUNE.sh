2e29856733799bafb0f69b24afccfb38b5a82729
pugdev <duduterrafogo1@proton.me>
2025-06-08 10:10:11 -0300

    feat: Add port files

---
+++ DELTARUNE.sh
@@ -0,0 +1,121 @@
+#!/bin/bash
+if ! command -v inotifywait 2>&1 >/dev/null
+then
+	echo "inotifywait not found. Please install inotify-tools using your package manager"
+	exit 1
+fi
+
+if ! command -v mpv 2>&1 >/dev/null
+then
+	echo "mpv not found. Please install mpv using your package manager"
+	exit 1
+fi
+
+DELTARUNEDIR=$(pwd)
+SAVEDIR="$HOME/.config/DELTARUNE"
+
+CHAPTERSELECT_FILE="$SAVEDIR/deltarune_chapterselect"
+CHAPTER1_FILE="$SAVEDIR/deltarune_chapter1"
+CHAPTER2_FILE="$SAVEDIR/deltarune_chapter2"
+CHAPTER3_FILE="$SAVEDIR/deltarune_chapter3"
+CHAPTER4_FILE="$SAVEDIR/deltarune_chapter4"
+
+VIDEO_ENG_FILE="$SAVEDIR/play_video_en"
+VIDEO_JAP_FILE="$SAVEDIR/play_video_jp"
+
+# Just in case
+mkdir -p $SAVEDIR
+
+# Check if trigger files are somehow there and delete them
+if [ -f "$CHAPTERSELECT_FILE" ]; then
+	# Scary! owo
+	rm -fr $CHAPTERSELECT_FILE
+fi
+
+if [ -f "$CHAPTER1_FILE" ]; then
+	rm -fr $CHAPTER1_FILE
+fi
+
+if [ -f "$CHAPTER2_FILE" ]; then
+	rm -fr $CHAPTER2_FILE
+fi
+
+if [ -f "$CHAPTER3_FILE" ]; then
+	rm -fr $CHAPTER3_FILE
+fi
+
+if [ -f "$CHAPTER4_FILE" ]; then
+	rm -fr $CHAPTER4_FILE
+fi
+
+function run_game {
+	LD_LIBRARY_PATH="$DELTARUNEDIR/lib" "./deltarune" &
+}
+
+# Run the game! 
+run_game
+
+# All the logic for changing chapters (and playing video) -=
+
+# Remove trigger files (just in case)
+if [ -f "$VIDEO_ENG_FILE" ]; then
+	rm -fr $VIDEO_ENG_FILE
+fi
+
+if [ -f "$VIDEO_JAP_FILE" ]; then
+	rm -fr $VIDEO_JAP_FILE
+fi
+
+parse_file() {
+	if [ "$1" == "deltarune_chapterselect" ]; then
+		rm -fr $2/$1
+		change_chapter 0
+	elif [ "$1" == "deltarune_chapter1" ]; then
+		rm -fr $2/$1
+		change_chapter 1
+	elif [ "$1" == "deltarune_chapter2" ]; then
+		rm -fr $2/$1
+		change_chapter 2
+    	elif [ "$1" == "deltarune_chapter3" ]; then
+		rm -fr $2/$1
+		change_chapter 3
+	elif [ "$1" == "deltarune_chapter4" ]; then
+		rm -fr $2/$1
+		change_chapter 4
+    elif [ "$1" == "play_video_en" ]; then
+        rm -fr "$VIDEO_ENG_FILE"
+        mpv --sub-file="$DELTARUNEDIR/chapter3_linux/assets/vid/subtitles.srt" --ontop=yes --fullscreen "$DELTARUNEDIR/chapter3_linux/assets/vid/tennaintrof1_compressed_28.mp4"
+    elif [ "$1" == "play_video_jp" ]; then
+        rm -fr "$VIDEO_JAP_FILE"
+        mpv --sub-file="$DELTARUNEDIR/chapter3_linux/assets/vid/subtitles.srt" --ontop=yes --fullscreen "$DELTARUNEDIR/chapter3_linux/assets/vid/tennaintrojpf1_compressed_28.mp4"
+	else
+		return
+	fi
+}
+
+change_chapter() {
+	if [ "$1" == "0" ]; then
+		cd ".."
+		run_game
+	elif [ "$1" == "1" ]; then
+		cd "$DELTARUNEDIR/chapter1_linux"
+		run_game
+	elif [ "$1" == "2" ]; then
+		cd "$DELTARUNEDIR/chapter2_linux"
+		run_game
+	elif [ "$1" == "3" ]; then
+		cd "$DELTARUNEDIR/chapter3_linux"
+		run_game
+	elif [ "$1" == "4" ]; then
+		cd "$DELTARUNEDIR/chapter4_linux"
+		run_game
+	fi
+}
+
+# Watch the game save directory for trigger files
+inotifywait -m $SAVEDIR |
+	while read filepath operation file; do
+		[[ $operation == *CREATE* ]] && parse_file $file $filepath
+	done
+
+
---
+++ port.sh
@@ -0,0 +1,130 @@
+#!/bin/bash
+if ! command -v rsync 2>&1 >/dev/null
+then
+	echo "rsync not found. Please install rsync using your package manager"
+	exit 1
+fi
+
+if ! command -v xdelta 2>&1 >/dev/null
+then
+	echo "xdelta not found. Please install xdelta using your package manager"
+	exit 1
+fi
+
+DELTARUNEDIR=""
+SCRIPTDIR=$(pwd)
+
+function port_game() {
+   echo ""
+   cd $DELTARUNEDIR
+
+   echo -e "\e[1;34m::\e[0m \e[1mRenaming files\e[0m"
+   find -name "chapter*" | sed 'p;s/windows/linux/' | xargs -d '\n' -n 2 mv
+   find "mus/" -exec bash -c 'mv $0 ${0,,}' {} \;
+   find "chapter3_linux/vid/" -exec bash -c 'mv $0 ${0,,}' {} \;
+   mv "$DELTARUNEDIR/chapter1_linux/AUDIO_INTRONOISE.ogg" "$DELTARUNEDIR/chapter1_linux/audio_intronoise.ogg"
+   mv "$DELTARUNEDIR/chapter2_linux/AUDIO_INTRONOISE.ogg" "$DELTARUNEDIR/chapter2_linux/audio_intronoise.ogg" 
+   mv "$DELTARUNEDIR/chapter3_linux/AUDIO_INTRONOISE.ogg" "$DELTARUNEDIR/chapter3_linux/audio_intronoise.ogg" 
+   mv "$DELTARUNEDIR/chapter4_linux/AUDIO_INTRONOISE.ogg" "$DELTARUNEDIR/chapter4_linux/audio_intronoise.ogg"
+   echo ""
+
+   echo -e "\e[1;34m::\e[0m \e[1mMoving files...\e[0m"
+   rm -fr DELTARUNE.exe
+   rm -fr options.ini
+   mv data.win game.unx
+    
+   cp "$SCRIPTDIR/deltarune" .
+   cp "$SCRIPTDIR/DELTARUNE.sh" .
+   cp "$SCRIPTDIR/icon.png" .
+   cp "$SCRIPTDIR/splash.png" .
+   cp -r "$SCRIPTDIR/files/lib" .
+
+   find . -type d -name "chapter*_linux" -print0 | while IFS= read -r -d $'\0' chapter_dir; do
+        rm -fr "$chapter_dir/options.ini"
+        mv "$chapter_dir/data.win" "$chapter_dir/game.unx"
+        
+        mkdir -p "$chapter_dir/assets"
+        rsync -vau --remove-source-files "$chapter_dir/" "$chapter_dir/assets"
+        rm -fr "$chapter_dir/assets/assets"
+        rm -fr "$chapter_dir/lang"
+
+        cp "$SCRIPTDIR/deltarune" "$chapter_dir/"
+        cp "$SCRIPTDIR/icon.png" "$chapter_dir/"
+        cp "$SCRIPTDIR/splash.png" "$chapter_dir/"
+        cp -r "$SCRIPTDIR/files/lib" "$chapter_dir/"
+        ln -sf "../../mus" "$chapter_dir/assets/mus"
+   done
+
+   cp "$SCRIPTDIR/files/options_1.ini" "$DELTARUNEDIR/chapter1_linux/options.ini"
+   cp "$SCRIPTDIR/files/options_2.ini" "$DELTARUNEDIR/chapter2_linux/options.ini"
+   cp "$SCRIPTDIR/files/options_3.ini" "$DELTARUNEDIR/chapter3_linux/options.ini"
+   cp "$SCRIPTDIR/files/options_4.ini" "$DELTARUNEDIR/chapter4_linux/options.ini"
+   cp "$SCRIPTDIR/files/subtitles.srt" "$DELTARUNEDIR/chapter3_linux/assets/vid/"
+   rm -fr "$DELTARUNEDIR/chapter3_linux/vid"
+
+   echo -e "\e[1;34m::\e[0m \e[1mApplying patches\e[0m"
+   echo ""
+   xdelta -d -s "$DELTARUNEDIR/game.unx" "$SCRIPTDIR/files/patches/01-menu.delta" "$DELTARUNEDIR/game.unx.1"
+   xdelta -d -s "$DELTARUNEDIR/chapter1_linux/assets/game.unx" "$SCRIPTDIR/files/patches/02-chapter_01.delta" "$DELTARUNEDIR/chapter1_linux/assets/game.unx.1"
+   xdelta -d -s "$DELTARUNEDIR/chapter2_linux/assets/game.unx" "$SCRIPTDIR/files/patches/03-chapter_02.delta" "$DELTARUNEDIR/chapter2_linux/assets/game.unx.1"
+   xdelta -d -s "$DELTARUNEDIR/chapter3_linux/assets/game.unx" "$SCRIPTDIR/files/patches/04-chapter_03.delta" "$DELTARUNEDIR/chapter3_linux/assets/game.unx.1"
+   xdelta -d -s "$DELTARUNEDIR/chapter3_linux/assets/game.unx.1" "$SCRIPTDIR/files/patches/05-chapter_03_video_fix.delta" "$DELTARUNEDIR/chapter3_linux/assets/game.unx.2"
+   xdelta -d -s "$DELTARUNEDIR/chapter4_linux/assets/game.unx" "$SCRIPTDIR/files/patches/06-chapter_04.delta" "$DELTARUNEDIR/chapter4_linux/assets/game.unx.1"
+
+   rm -fr "$DELTARUNEDIR/game.unx"
+   rm -fr "$DELTARUNEDIR/chapter1_linux/assets/game.unx"
+   rm -fr "$DELTARUNEDIR/chapter2_linux/assets/game.unx"
+   rm -fr "$DELTARUNEDIR/chapter3_linux/assets/game.unx"
+   rm -fr "$DELTARUNEDIR/chapter3_linux/assets/game.unx.1"
+   rm -fr "$DELTARUNEDIR/chapter4_linux/assets/game.unx"
+   mv "$DELTARUNEDIR/game.unx.1" "$DELTARUNEDIR/game.unx"
+   mv "$DELTARUNEDIR/chapter1_linux/assets/game.unx.1" "$DELTARUNEDIR/chapter1_linux/assets/game.unx"
+   mv "$DELTARUNEDIR/chapter2_linux/assets/game.unx.1" "$DELTARUNEDIR/chapter2_linux/assets/game.unx"
+   mv "$DELTARUNEDIR/chapter3_linux/assets/game.unx.2" "$DELTARUNEDIR/chapter3_linux/assets/game.unx"
+   mv "$DELTARUNEDIR/chapter4_linux/assets/game.unx.1" "$DELTARUNEDIR/chapter4_linux/assets/game.unx"
+
+   ln -sf "$DELTARUNEDIR/DELTARUNE.sh" "$DELTARUNEDIR/DELTARUNE.exe"
+
+   echo -e "\e[1;32m SUCCESS! The port script finished. \e[0m"
+   echo -e '\e[1;34m::\e[0m \e[1mTo play DELTARUNE, go to Steam -> DELTARUNE -> Properties -> Launch Options -> Put this: "./DELTARUNE.sh" %command% \e[0m'
+   echo -e "\e[1;34m::\e[0m \e[1mOr, you can run ./DELTARUNE.sh in the game folder.\e[0m"
+   echo -e "\e[1;34m::\e[0m \e[1mEnjoy and have fun! \e[0m"
+}
+
+function select_dir() {
+   echo ""
+   echo -e "\e[1;34m::\e[0m \e[1mPlease type the path of DELTARUNE below (eg. /home/pug/.local/share/Steam/steamapps/common/DELTARUNE):\e[0m"
+   read path
+
+   if [ "$path" = "" ]; then
+      exit;
+   fi
+
+   if [ ! -d "$path" ]; then
+      echo -e "\e[31mERROR: Directory dosen't exist, please try again\e[0m"
+      exit;
+   fi
+
+   DELTARUNEDIR=$path
+   port_game
+}
+
+echo -e "\e[1;34mWelcome to the unofficial DELTARUNE Chapter 3 & 4 Linux port!\e[0m"
+echo -e "\e[1;34mThis is for version 1.01A\e[0m"
+echo -e "\e[1;34mSince Chapter 3&4 are paid, you are gonna need to own a copy. This dosen't include any game data\e[0m"
+echo ""
+
+if [ -d "$HOME/.steam/steam/steamapps/common/DELTARUNE" ]; then
+	DELTARUNEDIR="$HOME/.steam/steam/steamapps/common/DELTARUNE"
+	echo -e "\e[1;34m::\e[0m \e[1mDetected deltarune directory at $DELTARUNEDIR."
+	while true; do
+		read -p "Is this correct? [y/n]: " yn
+		case $yn in
+			[Yy]* ) port_game; break;;
+			[Nn]* ) select_dir; break;;
+			* ) select_dir; break;;
+		    esac
+		done
+else
+	select_dir
+fi
